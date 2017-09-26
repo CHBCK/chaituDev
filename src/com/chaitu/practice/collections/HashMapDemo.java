989eb4a8b4f9c8a9f47146e58038f2032fb0aa1d
chaitu <krishna1133.ch@gmail.com>
2017-09-26 21:50:39 +0530

    added new java file

---
+++ src/com/chaitu/practice/collections/HashMapDemo.java
@@ -0,0 +1,15 @@
+package com.chaitu.practice.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		Map map = new HashMap();
+		map.put("1", "Chaitu");
+		map.put("2", "Sowji");
+		
+		System.out.println("Map values : "+ map);
+	}
+}
