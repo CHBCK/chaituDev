004ccac4846065709b65d9167bbcf83037c7a1b6
chaitu <krishna1133.ch@gmail.com>
2017-09-26 22:04:19 +0530

    added comment

--- src/com/chaitu/practice/collections/HashMapDemo.java
+++ src/com/chaitu/practice/collections/HashMapDemo.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class HashMapDemo {
 
 	public static void main(String[] args) {
+		// simple map usage
 		Map map = new HashMap();
 		map.put("1", "Chaitu");
 		map.put("2", "Sowji");
